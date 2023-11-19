f4e3e5d0820788eb592ed69cc54eed2320a3225d
MiroslavPK <miroslavkarakostov@gmail.com>
2023-11-19 15:34:48 +0200

    07-Advanced_Django_Models_Techniques - Solutions

---
+++ 07-Advanced_Django_Models_Techniques/main_app/validators.py
@@ -0,0 +1,15 @@
+from django.core.exceptions import ValidationError
+import re
+
+
+
+def validate_name(value):
+    for char in value:
+        if not (char.isalpha() or char.isspace()):
+            raise ValidationError("Name can only contain letters and spaces")
+
+
+def validate_phone_number(value):
+    if not re.match("^\+359\d{9}$", value):
+        raise ValidationError("Phone number must start with a '+359' followed by 9 digits")
+
